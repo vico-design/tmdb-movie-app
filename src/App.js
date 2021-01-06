f5f94dd04800f7fb97659a8c8091a2a6cec3152a
Victoria Petrone <65722111+vico-design@users.noreply.github.com>
2021-01-06 14:48:07 +0100

    added more styles to the row poster

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./styles.css";
 import Row from "./row";
 import requests from "./requests";
 import Banner from "./banner";
 import Navbar from "./navbar";
-import axios from "./axios";
 
 const App = () => {
-  //   const blabla = async () => {
-  //     const results = await axios.get(requests.fetchActionMovies);
-  //     console.log(results);
-  //   };
-  //   useEffect(() => {
-  //     blabla();
-  //   }, []);
-
   return (
     <div className="app">
       <Navbar />
       <Banner />
+      <Row title="Trending Now" fetchUrl={requests.fetchTopRated} />
       <Row
-        title="Netflix Originals"
+        title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
         isLargeRow
       />
-      <Row title="Trending Now" fetchUrl={requests.fetchTopRated} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
