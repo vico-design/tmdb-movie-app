583f5c8109d11563f9f1f2a7dd6780125f0edd39
Victoria Petrone <65722111+vico-design@users.noreply.github.com>
2021-01-15 17:20:56 +0100

    added gh pages to my app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Home from "./pages/home";
 import Search from "./pages/search";
@@ -9,17 +9,19 @@ import "./styles.css";
 
 const App = () => {
   return (
-    <div className="app-container">
-      <Navbar />
-      <div className="body-container">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/search" component={Search} />
-          <Route exact path="/detail" component={Detail} />
-        </Switch>
+    <HashRouter basename="/">
+      <div className="app-container">
+        <Navbar />
+        <div className="body-container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/detail" component={Detail} />
+          </Switch>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </HashRouter>
   );
 };
 
